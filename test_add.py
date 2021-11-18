93c61761f221c01ae65ca3f5e6bb164fda3086ff
mcieciora <michalcieciora96@gmail.com>
2021-11-18 15:58:04 +0100

    Unskipped tests

--- test_add.py
+++ test_add.py
@@ -1,38 +1,32 @@
 import pytest
 
 
-@pytest.mark.skip('Feature is not implemented yet')
 @pytest.mark.smoke
 def test_add__positive_int_values(calculator):
     assert calculator.add(1, 3) == 4
 
 
-@pytest.mark.skip('Feature is not implemented yet')
 @pytest.mark.smoke
 def test_add__positive_float_values(calculator):
     assert calculator.add(1.1, 3.5) == 4.6
 
 
-@pytest.mark.skip('Feature is not implemented yet')
 @pytest.mark.smoke
 def test_add__negative_int_values(calculator):
     assert calculator.add(-2, -3) == -5
 
 
-@pytest.mark.skip('Feature is not implemented yet')
 @pytest.mark.smoke
 def test_add__negative_float_values(calculator):
     assert calculator.add(-0.5, -2) == -2.5
 
 
-@pytest.mark.skip('Feature is not implemented yet')
 @pytest.mark.regression
 def test_add__mixed_int_values(calculator):
     assert calculator.add(1, -3) == -2
     assert calculator.add(-4, 3) == -1
 
 
-@pytest.mark.skip('Feature is not implemented yet')
 @pytest.mark.regression
 def test_add__mixed_float_values(calculator):
     assert calculator.add(1.4, -3.1) == -1.7
