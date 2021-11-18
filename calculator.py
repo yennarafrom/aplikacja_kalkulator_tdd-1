697c6c96bbf604c4d6dc908e6be48fa7966ea3ec
mcieciora <michalcieciora96@gmail.com>
2021-11-18 15:47:05 +0100

    Added calculator app

---
+++ calculator.py
@@ -0,0 +1,25 @@
+number_types = [int, float, complex]
+
+
+class Calculator:
+
+    @staticmethod
+    def validate_args(x, y):
+        if type(x) not in number_types and type(y) not in number_types:
+            raise ValueError
+
+    def add(self, x, y):
+        self.validate_args(x, y)
+        return x + y
+
+    def mul(self, x, y):
+        self.validate_args(x, y)
+        return x*y
+
+    def sub(self, x, y):
+        self.validate_args(x, y)
+        return x-y
+
+    def div(self, x, y):
+        self.validate_args(x, y)
+        return x/y
